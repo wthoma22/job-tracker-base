ac7ea80bdbac9be2abcc5d71ab64db48c3e8f174
William Thomas <wthoma390@gmail.com>
2017-06-06 18:13:41 -0600

    creates locations migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,15 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214181533) do
+ActiveRecord::Schema.define(version: 20170607000953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "job_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["job_id"], name: "index_comments_on_job_id", using: :btree
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "position"
+    t.string   "email"
+    t.integer  "company_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["company_id"], name: "index_contacts_on_company_id", using: :btree
   end
 
   create_table "jobs", force: :cascade do |t|
@@ -30,9 +53,28 @@ ActiveRecord::Schema.define(version: 20161214181533) do
     t.datetime "updated_at",        null: false
     t.integer  "company_id"
     t.string   "city"
+    t.index ["company_id"], name: "index_jobs_on_company_id", using: :btree
   end
 
-  add_index "jobs", ["company_id"], name: "index_jobs_on_company_id", using: :btree
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_taggings_on_job_id", using: :btree
+    t.index ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "jobs", "companies"
+  add_foreign_key "taggings", "tags"
 end
